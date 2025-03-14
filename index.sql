09fe90228acf1c3e4358187dbe9870b6d4e6fb45
Anastasia <99526512+zutilda@users.noreply.github.com>
2025-03-14 20:06:31 +0300

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,3 +1,4 @@
 CREATE INDEX idx_distributionlog_cargo_id ON DistributionLog(CargoId);
 CREATE INDEX idx_distributionlog_container_id ON DistributionLog(ContainerId);
 CREATE INDEX idx_distributionlog_warehouse_id ON DistributionLog(WarehouseId);
+create index idx_distributionlog_warehouse_name on warehouse(name) 
